80a1fa41625a86cb926b94981755b7c5cc80ba2a
Jorge Enríquez <44071711+jenriquez-bit@users.noreply.github.com>
2024-04-03 19:14:58 -0600

    Se crea una forma de executar un fake reponse (#2)

--- src/Traits/DataEntity/ExecutesQuery.php
+++ src/Traits/DataEntity/ExecutesQuery.php
@@ -5,7 +5,9 @@ namespace BitMx\DataEntities\Traits\DataEntity;
 use BitMx\DataEntities\DataEntity;
 use BitMx\DataEntities\Executers\Executer;
 use BitMx\DataEntities\PendingQuery;
+use BitMx\DataEntities\Responses\MockResponse;
 use BitMx\DataEntities\Responses\Response;
+use Illuminate\Support\Arr;
 
 /**
  * @mixin DataEntity
@@ -14,17 +16,47 @@ trait ExecutesQuery
 {
     public function execute(): Response
     {
+        return $this->resolveResponse();
+    }
+
+    protected function createPendingQuery(): PendingQuery
+    {
+        $pendingQuery = new PendingQuery($this);
+
+        return $pendingQuery;
+    }
+
+    protected function resolveResponse(): Response
+    {
+        if (static::isFake()) {
+            return $this->executeMockResponse();
+        }
+
+        return $this->executeQuery();
+    }
+
+    protected function executeMockResponse(): Response
+    {
+        if (! Arr::has(static::$mockResponses, get_class($this))) {
+            return $this->createFakeResponse(MockResponse::make([]));
+        }
+
+        $mockResponse = Arr::get(static::$mockResponses, get_class($this));
+
+        return $this->createFakeResponse($mockResponse);
+    }
+
+    protected function createFakeResponse(MockResponse $mockResponse): Response
+    {
+        return new Response($this, $mockResponse->data(), true);
+    }
+
+    protected function executeQuery(): Response
+    {
         $executer = new Executer($this->createPendingQuery());
 
         $reponse = $executer->exec();
 
         return $reponse;
-    }
-
-    public function createPendingQuery(): PendingQuery
-    {
-        $pendingQuery = new PendingQuery($this);
-
-        return $pendingQuery;
     }
 }
