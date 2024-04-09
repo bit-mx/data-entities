520fdeece18ba85bff43025d8af9add2616cd368
Jorge Enríquez <44071711+jenriquez-bit@users.noreply.github.com>
2024-04-09 17:34:58 -0600

    Add Data Entity Factory creation command
    The commit introduces a new console command to generate Data Entity Factory classes. This is supported by the addition of a new template stub for these factory classes. The new command was also added to the command list in the DataEntitiesServiceProvider.

---
+++ src/Commands/MakeDataEntityFactory.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace BitMx\DataEntities\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+
+class MakeDataEntityFactory extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:data-entity-factory {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a new data entity factory class';
+
+    protected string $namespace = 'Test\RequestFactories';
+
+    #[\Override]
+    protected function getStub(): string
+    {
+        return $this->getStubPath();
+    }
+
+    public function getStubPath(): string
+    {
+        return __DIR__.'/../../stubs/data-entity-factory.stub';
+    }
+
+    /**
+     * @param  string  $name
+     */
+    protected function getPath(mixed $name): string
+    {
+        $name = Str::replaceFirst($this->rootNamespace(), '', $name);
+
+        return base_path('tests').str_replace('\\', '/', $name).'.php';
+    }
+
+    /**
+     * Get the root namespace for the class.
+     */
+    protected function rootNamespace(): string
+    {
+        return 'Tests';
+    }
+
+    /**
+     * @param  string  $rootNamespace
+     */
+    protected function getDefaultNamespace(mixed $rootNamespace): string
+    {
+        return $rootNamespace.'\RequestFactories';
+    }
+}
